c7e476f3b2d094492f8f419b5810159c8b188d72
ericyu423 <ericyu423@hotmail.com>
2018-03-28 13:29:26 -0700

    collection view firebase update

---
+++ SocialMediaWithFirebase/AddPhoto/photoSelectorController.swift
@@ -0,0 +1,92 @@
+//
+//  photoSelectorController.swift
+//  SocialMediaWithFirebase
+//
+//  Created by eric yu on 3/28/18.
+//  Copyright © 2018 eric yu. All rights reserved.
+//
+
+import UIKit
+
+class PhotoSelectorController: UICollectionViewController,UICollectionViewDelegateFlowLayout{
+    let cellId = "cellId"
+    let headerId = "headerId"
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        collectionView?.backgroundColor = .yellow
+        
+        setupNavigationButton()
+        
+        //register cell
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+       
+        //register header
+         collectionView?.register(UICollectionViewCell.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerId)
+  
+    }
+    
+    
+    private func setupNavigationButton(){
+        navigationController?.navigationBar.tintColor = .black
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
+         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .plain, target: self, action: #selector(handleNext))
+    }
+    @objc private func handleCancel(){
+        dismiss(animated: true,completion: nil)
+    }
+    @objc private func handleNext(){
+        dismiss(animated: true,completion: nil)
+    }
+    
+}
+
+//collecdtionViewFlowLayout delegate
+extension PhotoSelectorController {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        //-3 because there are 3 black lines that are 1 pixel wide inbetween each collection view
+        let width = (view.frame.width - 3 ) / 4
+        return CGSize(width: width, height: width)
+    }
+}
+
+//collectionView delegate functions
+extension PhotoSelectorController {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 1, left: 0, bottom: 0, right: 0)
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        let width = view.frame.width
+        return CGSize(width: width, height: width)
+    }
+    //this function will show only if referenceSizeForFooter is called
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId, for: indexPath)
+        header.backgroundColor = .red
+        return header
+    }
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .blue
+        return cell
+    }
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+ 
+}
+
+
--- SocialMediaWithFirebase/Util/MainTabBarViewController.swift
+++ SocialMediaWithFirebase/Util/MainTabBarViewController.swift
@@ -9,11 +9,30 @@
 import UIKit
 import Firebase
 
-class MainTabBarViewController: UITabBarController {
+class MainTabBarViewController: UITabBarController,UITabBarControllerDelegate {
 
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController)-> Bool {
+        let index = viewControllers?.index(of: viewController)
+        if index == 2 {
+            
+            let layout = UICollectionViewFlowLayout()
+            let photoSelectorController = PhotoSelectorController(collectionViewLayout: layout)
+            
+            let navController = UINavigationController(rootViewController: photoSelectorController)
+            
+            
+            present(navController,animated: true,completion: nil)
+    
+            return false
+        }
+        return true
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.delegate = self
         if Auth.auth().currentUser == nil {
             DispatchQueue.main.async {
             let loginVC = LoginViewController()
@@ -28,20 +47,45 @@ class MainTabBarViewController: UITabBarController {
     
     //will be call to reset
     internal func setupViewControllers(){
+        
+        let homeNavController = templateNavController(unselectedImage: #imageLiteral(resourceName: "home_unselected"), selectedImage: #imageLiteral(resourceName: "home_selected"), rootViewController: UserProfileController(collectionViewLayout: UICollectionViewFlowLayout()))
+     
+        let searchNavController = templateNavController(unselectedImage: #imageLiteral(resourceName: "search_unselected"), selectedImage: #imageLiteral(resourceName: "search_selected"))
+        let plusNavController = templateNavController(unselectedImage: #imageLiteral(resourceName: "plus_unselected"), selectedImage: #imageLiteral(resourceName: "plus_unselected"))
+        let likeNavController = templateNavController(unselectedImage: #imageLiteral(resourceName: "like_unselected"), selectedImage: #imageLiteral(resourceName: "like_selected"))
+        
+  
         //collection view needs to initialized with layout
         let layout = UICollectionViewFlowLayout()
         let userProfileController = UserProfileController(collectionViewLayout: layout)
         
-        let navController = UINavigationController(rootViewController: userProfileController)
+        let userProfileNavController = UINavigationController(rootViewController: userProfileController)
         
-        navController.tabBarItem.image = #imageLiteral(resourceName: "profile_unselected")
-        navController.tabBarItem.selectedImage = #imageLiteral(resourceName: "profile_selected")
+        userProfileNavController.tabBarItem.image = #imageLiteral(resourceName: "profile_unselected")
+        userProfileNavController.tabBarItem.selectedImage = #imageLiteral(resourceName: "profile_selected")
         
         tabBar.tintColor = .black
         
         let SignUpVC = SignUpViewController()
         SignUpVC.view.backgroundColor = .white
-        viewControllers = [navController, SignUpVC]
+        
+        viewControllers = [homeNavController, searchNavController,plusNavController,likeNavController, userProfileNavController]
+        
+        guard let items = tabBar.items else { return }
+        
+        for item in items {
+            item.imageInsets = UIEdgeInsets(top: 4, left: 0, bottom: -4, right: 0)
+        }
+        
+        
+    }
+    
+    private func templateNavController(unselectedImage:UIImage,selectedImage:UIImage,rootViewController: UIViewController = UIViewController()) -> UINavigationController {
+        let viewController = rootViewController
+        let navController = UINavigationController(rootViewController: viewController)
+        navController.tabBarItem.image = unselectedImage
+        navController.tabBarItem.selectedImage = selectedImage
+        return navController
     }
 
  
